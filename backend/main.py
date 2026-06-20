14b117af5d0801786b5fe5cbeedb28991eba5067
Riya3024 <riyapanditstm@gmail.com>
2026-06-20 20:38:40 +0530

    final submission

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,21 @@ from project_brain import router as brain_router
 
 
 from timeline import router as timeline_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(brain_router)
 app.include_router(timeline_router)
